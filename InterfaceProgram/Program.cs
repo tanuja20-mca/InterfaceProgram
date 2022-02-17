0e54902dff3a04c6636432f80f0a61c0c8e574f7
Tanuja pandey <pandey.tanuja2015@gmail.com>
2022-02-17 13:02:39 +0530

    Add project files.

---
+++ InterfaceProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+interface Perform
+{
+    void add();
+    void display();
+}
+class Test : Perform
+{
+    public void add()
+    {
+        int num1=10,num2=20,result;
+
+       result= num1 + num2;
+
+        Console.WriteLine("Sum is :" + result);
+
+    }
+
+    public void display()
+    {
+        Console.WriteLine("Addition Operation is performed successFully");
+    }
+    public static void Main(string[] args)
+    {
+        Test t= new Test();
+        t.add();
+        t.display();
+    }
+}
